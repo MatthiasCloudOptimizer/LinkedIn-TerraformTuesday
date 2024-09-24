a9254908ef2b075dcf0979a4dbcf5ac9c4ec0d8b
Matthias Braun <matthias.braun@provectus.de>
2024-09-24 10:13:38 +0200

    Azure VNet erstellt

--- 2024/AzureVirtualNetwork/main.tf
+++ 2024/AzureVirtualNetwork/main.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
 
 # Azure resource group
 resource "azurerm_resource_group" "rg" {
-    name     = "rg-JumpServer-westeu"
+    name     = "rg-AdmJumpServer-dev-westeu"
     location = "West Europe"
     tags = {
         "Environment"               = "Dev" 
@@ -24,21 +24,23 @@ resource "azurerm_resource_group" "rg" {
 
 # Azure virtual network (VNet)
 resource "azurerm_virtual_network" "vnet" {
-    name                = "vnet-JumpServer-westeu"
+    name                = "vnet-AdmJumpServer-dev-westeu"
     location            = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
-    address_space       = ["10.0.0.0/16"]
+    address_space       = ["10.0.0.0/23"]
     dns_servers         = ["10.0.0.4", "10.0.0.5"]
-
-    # inline subnet
-    subnet {
-        name             = "snet-JumpServer-westeu"
-        address_prefixes = ["10.0.1.0/24"]
-    }
 
     tags = {
         "Environment"               = "Dev" 
         "Workload"                  = "Jump-Server" 
         "Workload_Department"       = "Business Services" 
     }
+}
+
+# Azure subnet (SNet)
+resource "azurerm_subnet" "snet" {
+    name                 = "snet-AdmJumpServer-dev-westeu"
+    resource_group_name  = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.vnet.name
+    address_prefixes     = ["10.0.1.0/24"]
 }
