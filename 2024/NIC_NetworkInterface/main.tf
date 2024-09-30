4d9ad76f8c38a2e60577fccdc3b19f5487c3d70f
Matthias Braun <matthias.braun@provectus.de>
2024-09-30 11:32:40 +0200

    Add NIC

--- 2024/NIC_NetworkInterface/main.tf
+++ 2024/NIC_NetworkInterface/main.tf
@@ -48,3 +48,15 @@ resource "azurerm_subnet" "snet" {
     virtual_network_name = azurerm_virtual_network.vnet.name
     address_prefixes     = ["10.0.1.0/24"]
 }
+# Azure Network Interface (NIC)
+resource "azurerm_network_interface" "nic" {
+    name                = "nic-AdmJumpServer-dev-westeu"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    ip_configuration {
+        name                          = "ipconf-vm${local.vmName}"
+        subnet_id                     = azurerm_subnet.snet.id
+        private_ip_address_allocation = "Dynamic"
+    }
+}
